d781ce002fc57e9507bd5bc8c88d874ba6a3051d
orlando <aorlando2889@gmail.com>
2024-05-05 11:34:23 +0000

    store and cart functionalities

--- store/models.py
+++ store/models.py
@@ -29,7 +29,32 @@ class Product(models.Model):
     def __str__(self):
         return self.product_name
 
+# cette classe a été créé pour filtrer le queryset défini pour la gestion des couleurs et des tailles
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active=True)
+    
+    def sizes(self):
+        return super(VariationManager, self).filter(variation_category='size', is_active=True)
 
+
+
+class Variation(models.Model):
+    variations_category_choice = [
+        ('color', 'color'),
+        ('size', 'size'),
+    ]
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variations_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+
+    objects = VariationManager()
+
+    def __str__(self):
+        return self.variation_value
 
 
 
